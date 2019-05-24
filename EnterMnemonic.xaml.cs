56eb9e7e72cb935608e0b352300e9bbcdcec9423
Gareos86 <50965851+Gareos86@users.noreply.github.com>
2019-05-24 18:43:28 +0200

    First upload

---
+++ EnterMnemonic.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPFTextBoxAutoComplete;
+using NBitcoin;
+using System.Collections;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace FB_BIP44
+{
+    /// <summary>
+    /// Interaktionslogik für EnterMnemonic.xaml
+    /// </summary>
+    public partial class EnterMnemonic : Window
+    {
+        public Wordlist wordList;
+        public string selectedLanguage;
+        public int i;
+
+        public EnterMnemonic(int _i, string _selectedLanguage)
+        {
+            InitializeComponent();
+            selectedLanguage = _selectedLanguage;
+            i = _i;
+            lblWord.Content = "Enter word #" + _i;
+            lblSelectedWord.Content= "Selected word #" + _i;
+        }
+
+        private void txtInput_KeyUp(object sender, KeyEventArgs e)
+        {
+            bool found = false;
+            var border = (txtSuggestions.Parent as ScrollViewer).Parent as Border;
+
+            string query = (sender as TextBox).Text;
+
+            if (query.Length == 0)
+            {
+                // Clear   
+                txtSuggestions.Children.Clear();
+                border.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                border.Visibility = System.Windows.Visibility.Visible;
+            }
+
+            // Clear the list   
+            txtSuggestions.Children.Clear();
+
+            // Add the result   
+            var functions = new Functions();
+            wordList = functions.SelectedLanguage(selectedLanguage);
+
+            foreach (var obj in wordList.GetWords())
+            {
+                if (obj.ToLower().StartsWith(query.ToLower()))
+                {
+                    // The word starts with this... Autocomplete must work   
+                    addItem(obj);
+                    found = true;
+                }
+            }
+
+            if (!found)
+            {
+                txtSuggestions.Children.Add(new TextBlock() { Text = "No words found." });
+            }
+        }
+
+        private void addItem(string text)
+        {
+            TextBlock block = new TextBlock();
+
+            // Add the text   
+            block.Text = text;
+
+            // A little style...   
+            block.Margin = new Thickness(2, 3, 2, 3);
+            block.Cursor = Cursors.Hand;
+
+            // Mouse events   
+            block.MouseLeftButtonUp += (sender, e) =>
+            {
+                txtOutput.Text = (sender as TextBlock).Text;
+
+                if (i != 24)
+                {
+                    btnNext.IsEnabled = true;
+                }
+
+                if (i == 12 || i == 15 || i == 18 || i == 21 || i == 24)
+                {
+                    btnClose.IsEnabled = true;
+                }
+            };
+
+            block.MouseEnter += (sender, e) =>
+            {
+                TextBlock b = sender as TextBlock;
+                b.Background = Brushes.PeachPuff;
+            };
+
+            block.MouseLeave += (sender, e) =>
+            {
+                TextBlock b = sender as TextBlock;
+                b.Background = Brushes.Transparent;
+            };
+
+            // Add to the panel   
+            txtSuggestions.Children.Add(block);
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtOutput.Text.Length == 0)
+            {
+                MessageBox.Show("Please add the last word before closing this window.", "Missing word", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                DialogResult = false;
+            }
+        }
+    }
+}
---
+++ GenerateWallets.xaml.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Windows;
+using System.Security;
+using NBitcoin;
+using Nethereum.HdWallet;
+
+namespace FB_BIP44
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    class Functions
+    {
+        public Wordlist SelectedLanguage(string language)
+        {
+            Wordlist wordList = Wordlist.English;
+
+            switch (language)
+            {
+                case "Chinese Simplified":
+                    wordList = Wordlist.ChineseSimplified;
+                    break;
+                case "Chinese Traditional":
+                    wordList = Wordlist.ChineseTraditional;
+                    break;
+                case "English":
+                    wordList = Wordlist.English;
+                    break;
+                case "French":
+                    wordList = Wordlist.French;
+                    break;
+                case "Japanese":
+                    wordList = Wordlist.Japanese;
+                    break;
+                case "Spanish":
+                    wordList = Wordlist.Spanish;
+                    break;
+            }
+
+            return wordList;
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        public Wordlist wordList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cboLanguage.Items.Add("Chinese Simplified");
+            cboLanguage.Items.Add("Chinese Traditional");
+            cboLanguage.Items.Add("English");
+            cboLanguage.Items.Add("French");
+            cboLanguage.Items.Add("Japanese");
+            cboLanguage.Items.Add("Portuguese Brazil");
+            cboLanguage.Items.Add("Spanish");
+            cboLanguage.SelectedValue = "English";
+
+            cboCoin.Items.Add("BTC - Bitcoin");
+            cboCoin.Items.Add("ETH - Ethereum");
+            cboCoin.SelectedValue = "BTC - Bitcoin";
+
+            cboType.Items.Add("0 - Receiving");
+            cboType.Items.Add("1 - Change");
+            cboType.SelectedValue = "0 - Receiving";
+
+            for (int i = 0; i < 100; i++)
+            {
+                cboAccount.Items.Add(i);
+                cboIndex.Items.Add(i);
+            }
+            cboAccount.SelectedValue = 0;
+            cboIndex.SelectedValue = 0;
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+            SecureString secureStr = new SecureString();
+            for (int i = 0; i < txtRandom.ToString().Length; i++)
+            {
+                secureStr.AppendChar(txtRandom.ToString()[i]);
+            }
+            secureStr.MakeReadOnly();
+
+            var functions = new Functions();
+            wordList = functions.SelectedLanguage(cboLanguage.SelectedValue.ToString());
+            RandomUtils.AddEntropy(secureStr.ToString());
+            Mnemonic mnemo = new Mnemonic(wordList, WordCount.TwentyFour);
+            txtMnemonic.Text = mnemo.ToString();
+            btnCalculate.IsEnabled = true;
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            var functions = new Functions();
+            wordList = functions.SelectedLanguage(cboLanguage.SelectedValue.ToString());
+            Mnemonic mnemo = new Mnemonic(txtMnemonic.Text, wordlist: wordList);
+            ExtKey hdRoot = new ExtKey();
+            hdRoot = mnemo.DeriveExtKey(txtPasswort.Text);
+            txtRoot.Text = hdRoot.ToString(Network.Main);
+
+            var wallet = new Wallet(txtMnemonic.Text, txtPasswort.Text);
+            txtSeed.Text = wallet.Seed;
+
+            switch(cboCoin.SelectedValue)
+            {
+                case "BTC - Bitcoin":
+                    ExtKey key = hdRoot.Derive(new NBitcoin.KeyPath("m/44'/0'/" + cboAccount.SelectedValue + "'/" + Convert.ToString(cboType.SelectedValue).Substring(0, 1) + "/" + cboIndex.SelectedValue));
+                    ExtPubKey pubKey = key.Neuter();
+                    txtAddress.Text = key.PrivateKey.PubKey.ToString(Network.Main);
+                    txtPublicKey.Text = Convert.ToString(pubKey.PubKey.ScriptPubKey).Substring(0, 66);
+                    txtExtPublicKey.Text = pubKey.ToString(Network.Main);
+                    txtPrivateKey.Text = key.PrivateKey.ToString(Network.Main);
+                    break;
+
+                case "ETH - Ethereum":
+                    int index = Convert.ToInt32(cboIndex.SelectedValue);
+                    var account = wallet.GetAccount(index);
+                    txtAddress.Text = account.Address;
+                    txtPublicKey.Text = "";
+                    txtExtPublicKey.Text = "";
+                    txtPrivateKey.Text = account.PrivateKey;
+                    break;
+            }
+        }
+
+        private void btnCopyAddress_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(txtAddress.Text);
+        }
+
+        private void btnCopyPublicKey_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(txtPublicKey.Text);
+        }
+
+        private void btnCopyExtPublicKey_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(txtExtPublicKey.Text);
+        }
+
+        private void btnCopyPrivateKey_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Please be careful - Private Keys should never be stored electronically and should only be used in a secure environment!", "Attention", MessageBoxButton.OK, MessageBoxImage.Warning);
+            Clipboard.SetText(txtPrivateKey.Text);
+        }
+
+        public string Words;
+
+        private void btnExisting_Click(object sender, RoutedEventArgs e)
+        {
+            btnCalculate.IsEnabled = true;
+            Words = "";
+
+            for (int i = 1; i <= 24; i++)
+            {
+                EnterMnemonic dialog = new EnterMnemonic(i, cboLanguage.SelectedValue.ToString());
+
+                dialog.ShowDialog();
+
+                if (dialog.DialogResult.HasValue && dialog.DialogResult.Value)
+                {
+                    if (Words.Length == 0)
+                    {
+                        Words = dialog.txtOutput.Text;
+                    }
+                    else
+                    {
+                        Words = Words + " " + dialog.txtOutput.Text;
+                    }
+                }
+                else
+                {
+                    if (Words.Length == 0)
+                    {
+                        Words = dialog.txtOutput.Text;
+                    }
+                    else
+                    {
+                        Words = Words + " " + dialog.txtOutput.Text;
+                    }
+
+                    txtMnemonic.Text = Words;
+                    break;
+                }
+            }
+        }
+    }
+}
